a05184d5366039acd70c00f4a711d67bfe9e3e76
Akindolie Tayo <akindolietayo@gmail.com>
2021-05-06 15:39:25 +0100

    chore: add getBuildURL helper

--- functions/deploy.js
+++ functions/deploy.js
@@ -1,5 +1,9 @@
 const axios = require("axios");
 
+const getBuildURL = (hash, branch) => {
+  return `https://api.netlify.com/build_hooks/${hash}?trigger_branch=${branch}&trigger_title=PR-preview-for-${branch}`;
+};
+
 // will fire from a bitbucket webhook (create and update PR)
 // this function will get the source branch from PR
 // and send a post request to a netlify build hook
@@ -21,11 +25,11 @@ exports.handler = async function (event, context) {
 
     switch (repo) {
       case process.env.CAP_BITBUCKET_REPOSITORY_NAME:
-        url = `https://api.netlify.com/build_hooks/${process.env.CAP_NETLIFY_BUILD_HASH}?trigger_branch=${branch}&trigger_title=PR-preview-for-${branch}`;
+        url = getBuildURL(process.env.CAP_NETLIFY_BUILD_HASH, branch);
         break;
 
       case process.env.ECOM_BITBUCKET_REPOSITORY_NAME:
-        url = `https://api.netlify.com/build_hooks/${process.env.ECOM_NETLIFY_BUILD_HASH}?trigger_branch=${branch}&trigger_title=PR-preview-for-${branch}`;
+        url = getBuildURL(process.env.ECOM_NETLIFY_BUILD_HASH, branch);
         break;
 
       default:
--- functions/deployed.js
+++ functions/deployed.js
@@ -1,7 +1,6 @@
-const axios = require('axios');
-const NetlifyAPI = require('netlify');
+const axios = require("axios");
+const NetlifyAPI = require("netlify");
 const netlifyClient = new NetlifyAPI(process.env.NETLIFY_API_TOKEN);
-
 
 // fires when the netlify deployment is complete
 // triggered from netlify outgoing webhook notification for successful deploy
@@ -12,49 +11,48 @@ const netlifyClient = new NetlifyAPI(process.env.NETLIFY_API_TOKEN);
 // 4. use repo URL to query BB API for pull requests
 // 5 if there are PRs - add comments with deploy URL
 exports.handler = async function (event, context) {
-    try {
-        const body = JSON.parse(event.body)
-		console.log(body);
-		// 1 get site id
-		const siteId = body.site_id;
-		// 2 get site details
-		const siteDetails = await netlifyClient.getSite({ site_id: siteId });
-		// 3 get repo url
-		const repoUrl = siteDetails.build_settings.repo_url;
-		// 4 get pull requests
-		const repoName = repoUrl.split('https://bitbucket.org/')[1];
-		const bitbucketAPIBaseURL = `https://api.bitbucket.org/2.0/repositories/${repoName}`;
-		const bitbucketClient = axios.create({
-			baseURL: bitbucketAPIBaseURL,
-			auth: {
-				username: process.env.BITBUCKET_USER,
-				password: process.env.BITBUCKET_PASS,
-			},
-		});
+  try {
+    const body = JSON.parse(event.body);
+    console.log(body);
+    // 1 get site id
+    const siteId = body.site_id;
+    // 2 get site details
+    const siteDetails = await netlifyClient.getSite({ site_id: siteId });
+    // 3 get repo url
+    const repoUrl = siteDetails.build_settings.repo_url;
+    // 4 get pull requests
+    const repoName = repoUrl.split("https://bitbucket.org/")[1];
+    const bitbucketAPIBaseURL = `https://api.bitbucket.org/2.0/repositories/${repoName}`;
+    const bitbucketClient = axios.create({
+      baseURL: bitbucketAPIBaseURL,
+      auth: {
+        username: process.env.BITBUCKET_USER,
+        password: process.env.BITBUCKET_PASS,
+      },
+    });
 
-		const pullRequests = await bitbucketClient.get(
-			`/pullrequests?q=source.branch.name="${body.branch}" and state="OPEN"`
-		);
+    const pullRequests = await bitbucketClient.get(
+      `/pullrequests?q=source.branch.name="${body.branch}" and state="OPEN"`
+    );
 
-		// 5 - add comment with deploy url for each pr
-		const buildID = body.id;
-		const name = body.name;
-		const buildURL = `https://${buildID}--${name}.netlify.app`;
-		console.log('build url', buildURL)
-		if (pullRequests.data.size > 0) {
-			const commentRequests = pullRequests.data.values.map(({ id }) => {
-				return bitbucketClient.post(`/pullrequests/${id}/comments`, {
-                     content: { raw: `preview URL [${buildURL}](${buildURL})` } ,
-				});
-			});
+    // 5 - add comment with deploy url for each pr
+    const buildID = body.id;
+    const name = body.name;
+    const buildURL = `https://${buildID}--${name}.netlify.app`;
+    console.log("build url", buildURL);
+    if (pullRequests.data.size > 0) {
+      const commentRequests = pullRequests.data.values.map(({ id }) => {
+        return bitbucketClient.post(`/pullrequests/${id}/comments`, {
+          content: { raw: `preview URL [${buildURL}](${buildURL})` },
+        });
+      });
 
-			await Promise.all(commentRequests);
-            return { statusCode: 200, body: "comments added" }
-		}
+      await Promise.all(commentRequests);
+      return { statusCode: 200, body: "comments added" };
+    }
 
-		return { statusCode: 200, body: `No PRs found for branch ${body.branch}` };
-        
-	} catch (e) {
-		return { statusCode: 200, body: e.toString() };
-	}
+    return { statusCode: 200, body: `No PRs found for branch ${body.branch}` };
+  } catch (e) {
+    return { statusCode: 200, body: e.toString() };
+  }
 };
